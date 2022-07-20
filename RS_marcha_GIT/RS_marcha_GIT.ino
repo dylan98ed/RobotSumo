0137d9ac83e2e0aa0017ae28f0d65e6198fdfc70
xRobin16 <evee.16.castro@gmail.com>
2022-07-19 22:32:44 -0300

    modificaciones en: nombre de variables, espacios entre parentesis y llaves

--- RS_marcha_GIT/RS_marcha_GIT.ino
+++ RS_marcha_GIT/RS_marcha_GIT.ino
@@ -1,86 +1,89 @@
 
 //declaro variables..
-int OUT1 = 6;    //"OUT1" es un nombre que vos elegis, 5 es el numero de pin de la placa que va a ejecutar la accion
-int OUT2 = 7;
-int OUT3 = 8;
-int OUT4 = 9;
-// 5 y 6 motor 1 --- MOTOR IZQ
-// 7 y 7 motor 2 --- MOTOR DER
+int MOT1_C1 = 6;    //"MOT1_C1" es un nombre que vos elegis, 5 es el numero de pin de la placa que va a ejecutar la accion
+int MOT1_C2 = 7;
+int MOT2_C1 = 8;
+int MOT2_C2 = 9;
+// MOT 1 --- MOTOR IZQ
+// MOT 2 --- MOTOR DER
 
-int IN1 = 2; //sensor de linea Frente-Derecho
-int IN2 = 3; //sensor de linea Trasero-Derecho
-int IN3 = 4; //sensor de linea Frente-Izquierdo
-int IN4 = 5; //sensor de linea Trasero-Izquierdo
+int SENS_LIN_FDER = 2; //sensor de linea Frente-Derecho
+int SENS_LIN_TDER = 3; //sensor de linea Trasero-Derecho
+int SENS_LIN_FIZQ = 4; //sensor de linea Frente-Izquierdo
+int SENS_LIN_TIZQ = 5; //sensor de linea Trasero-Izquierdo
 
-const int Trigger = 10;   //Pin digital 10 para el Trigger del sensor
-const int Echo = 11;   //Pin digital 11 para el Echo del sensor
+const int SENS_ULTRASON_TRIGGER = 10;   //Pin digital 10 para el TRIGGER del sensor ultrasonido
+const int SENS_ULTRASON_ECHO = 11;   //Pin digital 11 para el ECHO del sensor ultrasonido
 
 char comando = 's', modo = 'a'; //comando y modo iniciales -- MOTOR PARADO (S) Y MODO AUTOMATICO (A)
 char aux; //variable auxiliar para actualizar el comando entrante por el monitor serie
 
-int detectado = 1; //DETECCION DE LINEA BLANCA = 0
+int LINEA_DETECTADA = 1; //DETECCION DE LINEA BLANCA = 0
 
 const int LLAVE = 12;
 
 
-void setup() {
+void setup() 
+{
 
   pinMode(LLAVE, INPUT); //SI CONECTAMOS ESTA LLAVE A 5V EL MOTOR AVANZA ATR
   
   //seteo pines de entrada de motores
-  pinMode(OUT1, OUTPUT); //motor1
-  pinMode(OUT2, OUTPUT); //motor1
-  pinMode(OUT3, OUTPUT); //motor2
-  pinMode(OUT4, OUTPUT); //motor2
+  pinMode(MOT1_C1, OUTPUT); //motor1
+  pinMode(MOT1_C2, OUTPUT); //motor1
+  pinMode(MOT2_C1, OUTPUT); //motor2
+  pinMode(MOT2_C2, OUTPUT); //motor2
 
-  pinMode(IN1, INPUT); //sensor de linea Frente-Derecho
-  pinMode(IN2, INPUT); //sensor de linea Trasero-Derecho
-  pinMode(IN3, INPUT); //sensor de Frente-Izquierdo
-  pinMode(IN4, INPUT); //sensor de Trasero-Izquierdo
+  pinMode(SENS_LIN_FDER, INPUT); //sensor de linea Frente-Derecho
+  pinMode(SENS_LIN_TDER, INPUT); //sensor de linea Trasero-Derecho
+  pinMode(SENS_LIN_FIZQ, INPUT); //sensor de Frente-Izquierdo
+  pinMode(SENS_LIN_TIZQ, INPUT); //sensor de Trasero-Izquierdo
   
   //seteo pines de entrada del sensor ultrasonido
-  pinMode(Trigger, OUTPUT); //pin como salida
-  pinMode(Echo, INPUT);  //pin como entrada
-  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+  pinMode(SENS_ULTRASON_TRIGGER, OUTPUT); //pin como salida
+  pinMode(SENS_ULTRASON_ECHO, INPUT);  //pin como entrada
+  digitalWrite(SENS_ULTRASON_TRIGGER, LOW);//Inicializamos el pin con 0
 
   Serial.begin(9600); //Seteo los baudios para comunicarme con el monitor serial. 9600 es la vieja confiable.
 
-  delay (5000); //5 segundos iniciales de quietud
+  delay(5000); //5 segundos iniciales de quietud
 }
 
 void loop() 
 { 
-  for (;;) { //Función loop() está anidado en un ciclo externo con algunas comprobaciones adicionales -- optimiza tiempo si la salteamos
+  for(;;) 
+  { //Función loop() está anidado en un ciclo externo con algunas comprobaciones adicionales -- optimiza tiempo si la salteamos
 
   
-    delay (1000);
+    delay(1000);
     
       aux = Serial.read();
-      if (aux == 'm' || aux == 'c' || aux == 's' || aux == 'd' || aux == 'i' || aux == 'w' || aux == 'a'){
+      if(aux == 'm' || aux == 'c' || aux == 's' || aux == 'd' || aux == 'i' || aux == 'w' || aux == 'a')
+      {
         comando = aux;
-      }
+        }
     
       //ejecuto funcion CASE
       comandos(comando);
     
-      if (digitalRead(LLAVE) == HIGH)
+      if(digitalRead(LLAVE) == HIGH)
       {
         comandos('m');
         }
         else
         {
-          if (modo == 'a' )
+          if(modo == 'a' )
           {
-            if (checkSenLineaTodos () == detectado)
+            if(checkSenLineaTodos () == LINEA_DETECTADA)
             {
               comandos('s');
               Serial.println("LINEA BLANCA A LA VISTA");
-              checkSensorLinea ();
+              checkSensorLinea();
               }
               else 
               {
                 comandos('m');
-                checkEnemy ();
+                checkEnemy();
                 }
             }
           }
@@ -117,16 +120,16 @@ void giroDer(int b1, int b2, int b3, int b4)
   contramarcha(b3,b4); //motor2 - der
 }
 
-long ultrasonido() 
+long medirDistancia() 
 {
   long t; //tiempo que demora en llegar el eco
   long d; //distancia en centimetros
 
-  digitalWrite(Trigger, HIGH);
+  digitalWrite(SENS_ULTRASON_TRIGGER, HIGH);
   delayMicroseconds(10);          //Enviamos un pulso de 10us
-  digitalWrite(Trigger, LOW);
+  digitalWrite(SENS_ULTRASON_TRIGGER, LOW);
   
-  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  t = pulseIn(SENS_ULTRASON_ECHO, HIGH); //obtenemos el ancho del pulso
   // 1/59 = 0.0169492
   d = t * 0.0169492;             //escalamos el tiempo a una distancia en cm
   
@@ -140,14 +143,15 @@ long ultrasonido()
   // return d;
 }
 
-long promedioDistancia ()
+long promedioDistancia()
 {
   long distancia, distancia_prom;
   long distancia_total = 0;
 
   //calculo de promedio de 10 distancias sensadas por el ultrasonido
-  for(int i = 10 ; i>0; i--){
-    distancia = ultrasonido(); 
+  for(int i = 10 ; i>0; i--)
+  {
+    distancia = medirDistancia(); 
     distancia_total = distancia_total + distancia;
   }
   // 1/10 = 0.1
@@ -162,13 +166,13 @@ long promedioDistancia ()
   }
 
 //busqueda del contrincante
-void checkEnemy ()
+void checkEnemy()
 { 
-  long distancia_promedio = promedioDistancia ();
+  long distancia_promedio = promedioDistancia();
   Serial.println("distancia promedio en CHECK ENEMY:");
   Serial.print(distancia_promedio);
   
-  if (distancia_promedio <= 75) // el ring tiene un area de 175cm o 154cm -- alrededor hay 100cm vacio
+  if(distancia_promedio <= 75) // el ring tiene un area de 175cm o 154cm -- alrededor hay 100cm vacio
   {
     Serial.println("enemigo al frente!");
     comandos('m');
@@ -180,13 +184,13 @@ void checkEnemy ()
       }
   }
 
-int checkSenLineaTodos () //VERIFICO SI ALGUN SENSOR DETECTA ALGO
+int checkSenLineaTodos() //VERIFICO SI ALGUN SENSOR DETECTA ALGO
 {
   int linea_cerca, sensor1, sensor2, sensor3, sensor4;
-  sensor1 = leerSensorLinea(IN1);
-  sensor2 = leerSensorLinea(IN2);
-  sensor3 = leerSensorLinea(IN3);
-  sensor4 = leerSensorLinea(IN4);
+  sensor1 = leerSensorLinea(SENS_LIN_FDER);
+  sensor2 = leerSensorLinea(SENS_LIN_TDER);
+  sensor3 = leerSensorLinea(SENS_LIN_FIZQ);
+  sensor4 = leerSensorLinea(SENS_LIN_TIZQ);
   linea_cerca = sensor1 & sensor2 & sensor3 & sensor4;
   Serial.println("resul linea cerca:");
   Serial.println(linea_cerca);
@@ -196,7 +200,7 @@ int checkSenLineaTodos () //VERIFICO SI ALGUN SENSOR DETECTA ALGO
   }
 
 //leo sensor
-int leerSensorLinea (int sensor)
+int leerSensorLinea(int sensor)
 {
   int value = 1;
   value = digitalRead(sensor);
@@ -204,41 +208,41 @@ int leerSensorLinea (int sensor)
   }
 
 //verifico cual sensor detecto la linea blanca
-void checkSensorLinea ()
+void checkSensorLinea()
 {
   /*
-   * int IN1 = 1; //sensor de linea Frente-Derecho
-      int IN2 = 2; //sensor de linea Trasero-Derecho
-      int IN3 = 3; //sensor de linea Frente-Izquierdo
-      int IN4 = 4; //sensor de linea Trasero-Izquierdo
+   * int SENS_LIN_FDER = 1; //sensor de linea Frente-Derecho
+      int SENS_LIN_TDER = 2; //sensor de linea Trasero-Derecho
+      int SENS_LIN_FIZQ = 3; //sensor de linea Frente-Izquierdo
+      int SENS_LIN_TIZQ = 4; //sensor de linea Trasero-Izquierdo
    */
   int sensorPin;
-  sensorPin = leerSensorLinea (IN1); //sensor de linea Frente-Derecho
-  if (sensorPin == detectado) //sensor de linea Frente-Derecho
+  sensorPin = leerSensorLinea(SENS_LIN_FDER); //sensor de linea Frente-Derecho
+  if(sensorPin == LINEA_DETECTADA) //sensor de linea Frente-Derecho
   {
     Serial.println("sensor de linea Frente-Derecho");
     reaccionFrenteDer();
     }
     else
     {
-      sensorPin = leerSensorLinea (IN2); //sensor de linea Trasero-Derecho
-      if (sensorPin == detectado) //sensor de linea Trasero-Derecho
+      sensorPin = leerSensorLinea(SENS_LIN_TDER); //sensor de linea Trasero-Derecho
+      if(sensorPin == LINEA_DETECTADA) //sensor de linea Trasero-Derecho
       {
         Serial.println("sensor de linea Trasero-Derecho");
         reaccionTraseroDer();
         }
         else 
         {
-          sensorPin = leerSensorLinea (IN3); //sensor de linea Frente-Izquierdo
-          if (sensorPin == detectado) //sensor de linea Frente-Izquierdo
+          sensorPin = leerSensorLinea(SENS_LIN_FIZQ); //sensor de linea Frente-Izquierdo
+          if(sensorPin == LINEA_DETECTADA) //sensor de linea Frente-Izquierdo
           {
             Serial.println("sensor de linea Frente-Izquierdo");
             reaccionFrenteIzq();
             }
             else
             {
-              sensorPin = leerSensorLinea (IN4); //sensor de linea Trasero-Izquierdo
-              if (sensorPin == detectado) //sensor de linea Trasero-Izquierdo
+              sensorPin = leerSensorLinea(SENS_LIN_TIZQ); //sensor de linea Trasero-Izquierdo
+              if(sensorPin == LINEA_DETECTADA) //sensor de linea Trasero-Izquierdo
               {
                 Serial.println("sensor de linea Trasero-Izquierdo");
                 reaccionTraseroIzq();
@@ -249,46 +253,46 @@ void checkSensorLinea ()
   }
 
 //reaccion a sensor frente-derecho
-void reaccionFrenteDer ()
+void reaccionFrenteDer()
 {
   comandos('c');
-  delay (2);
+  delay(2);
   comandos('i');
   tiempoGiro(45);
   comandos('m');
   }
 
 //reaccion a sensor frente-izquierdo
-void reaccionFrenteIzq ()
+void reaccionFrenteIzq()
 {
   comandos('c');
-  delay (2);
+  delay(2);
   comandos('d');
   tiempoGiro(45);
   comandos('m');
   }
 
 //reaccion a sensor trasero-derecho
-void reaccionTraseroDer ()
+void reaccionTraseroDer()
 {
   //hay que chekar!!
   
   //SI LO ESTAN EMPUJANDO Y ESTA POR CAERSE EN EL BORDE DERECHO -- DOBLA LAS RUEDAS PARA LA DER ?
   comandos('d');
-  delay (2);
+  delay(2);
   
   //UNA VEZ QUE SALE DE PELIGRO INTENTA TIRAR AL ENEMIGO GIRANDO PARA LA IZQUIERDA (asumo que van a quedar en paralelo mirando al borde -- el enemy estaria a la izq)
   comandos('i');
   }
 
 //reaccion a sensor trasero-izquierdo
-void reaccionTraseroIzq ()
+void reaccionTraseroIzq()
 {
   //hay que chekar!!
   
   //SI LO ESTAN EMPUJANDO Y ESTA POR CAERSE EN EL BORDE IZQUIERDO -- DOBLA LAS RUEDAS PARA LA IZQ ?
   comandos('i');
-  delay (2);
+  delay(2);
   //UNA VEZ QUE SALE DE PELIGRO INTENTA TIRAR AL ENEMIGO GIRANDO PARA LA DERECHA (asumo que van a quedar en paralelo mirando al borde -- el enemy estaria a la der)
   comandos('d');
   }
@@ -304,29 +308,30 @@ int tiempoGiro(int angulo) //calculo el tiempo que requiere el robot para girar 
 
 void comandos(char comando)
 {
-  switch (comando) {
+  switch(comando) 
+  {
     case 'm':                           //si pongo "m" en el monitor serial el motor avanza.
       Serial.println("marcha de motor");  //Serial.write() = escribe en el monitor serial.
-      marcha(OUT1,OUT2); //motor1
-      marcha(OUT3,OUT4); //motor2
+      marcha(MOT1_C1,MOT1_C2); //motor1
+      marcha(MOT2_C1,MOT2_C2); //motor2
       break;
     case 'c':                           //si pongo "c" en el monitor serial el motor retrocede.
       Serial.println("contramarcha de motor");
-      contramarcha(OUT1,OUT2); //motor1
-      contramarcha(OUT3,OUT4); //motor2
+      contramarcha(MOT1_C1,MOT1_C2); //motor1
+      contramarcha(MOT2_C1,MOT2_C2); //motor2
       break;
     case 's':                           //si pongo "s" en el monitor serial el motor se detiene
       Serial.println("motor detenido");
-      parada(OUT1,OUT2); //motor1
-      parada(OUT3,OUT4); //motor2
+      parada(MOT1_C1,MOT1_C2); //motor1
+      parada(MOT2_C1,MOT2_C2); //motor2
       break;
     case 'd':                           //si pongo "d" en el monitor serial el der avanza, izq retrocede
       Serial.println("motor giro derecha hacia adelante");
-      giroDer(OUT1,OUT2,OUT3,OUT4);
+      giroDer(MOT1_C1,MOT1_C2,MOT2_C1,MOT2_C2);
       break;
     case 'i':                           //si pongo "i" en el monitor serial motor izq avanza, der retrocede
       Serial.println("motor giro izquierda hacia adelante");
-      giroIzq(OUT1,OUT2,OUT3,OUT4);
+      giroIzq(MOT1_C1,MOT1_C2,MOT2_C1,MOT2_C2);
       break;
     case 'w':                           //si pongo "w" en el monitor serial ACTIVO MODO MANUAL
       Serial.println("motor MODO MANUAL");
@@ -339,8 +344,8 @@ void comandos(char comando)
       break;
     default:                            //si no pongo ningun comando, el motor se detiene
       Serial.println("motor detenido");
-      parada(OUT1,OUT2); //motor1
-      parada(OUT3,OUT4); //motor2
+      parada(MOT1_C1,MOT1_C2); //motor1
+      parada(MOT2_C1,MOT2_C2); //motor2
       break;
   }
  }
